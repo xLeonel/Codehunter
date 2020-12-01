8a3a528a541f892212a943f27455d7296f3d4077
Pedro Henrique da Silva <pedro.silva301201@gmail.com>
2020-12-01 09:59:49 -0300

    up

--- Mobile/screens/PerfilScreen.tsx
+++ Mobile/screens/PerfilScreen.tsx
@@ -1,22 +1,32 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import * as React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { RootStackParamList } from '../types';
 
 export default function Perfil({
     navigation,
 }: StackScreenProps<RootStackParamList, 'NotFound'>) {
     return (
-        <View>
-            <Text style={styles.texto}>Olá Mundo!</Text>
+        <View style={styles.container}>
+            
+            <View style={{ alignItems: "flex-start", padding: 20}}>
+                <Text style={styles.titulo}>Nome</Text>
+                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 50}}>Nome do Usuário</Text>
+
+                <Text style={styles.titulo}>Curso</Text>
+                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 50}}>usuario.senai@gmail.com</Text>
+            </View>
         </View>
     );
 }
 
 // Apenas teste
 const styles = StyleSheet.create({
-    texto: {
-        color: 'black'
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
     },
-})
+    titulo: {
+        color: '#757272'
+    }
+});
