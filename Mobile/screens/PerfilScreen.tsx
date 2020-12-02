68c8df166181e50ecb9b254a2c1e87487770f40e
Pedro Henrique da Silva <pedro.silva301201@gmail.com>
2020-12-02 09:10:03 -0300

    Error

--- Mobile/screens/PerfilScreen.tsx
+++ Mobile/screens/PerfilScreen.tsx
@@ -1,5 +1,5 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { RootStackParamList } from '../types';
 
@@ -11,16 +11,21 @@ export default function Perfil({
             
             <View style={{ alignItems: "flex-start", padding: 20}}>
                 <Text style={styles.titulo}>Nome</Text>
-                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 50}}>Nome do Usuário</Text>
+                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 40}}>Nome do Usuário</Text>
 
                 <Text style={styles.titulo}>Curso</Text>
-                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 50}}>usuario.senai@gmail.com</Text>
+                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 40}}>Desenvolvimento de Sistemas</Text>
+
+                <Text style={styles.titulo}>E-mail</Text>
+                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 40}}>usuario.senai@gmail.com</Text>
+
+                <Text style={styles.titulo}>Senha</Text>
+                <Text style={{ fontSize: 19, fontStyle: "italic", marginBottom: 50}}>******</Text>
             </View>
         </View>
     );
 }
 
-// Apenas teste
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -28,5 +33,9 @@ const styles = StyleSheet.create({
     },
     titulo: {
         color: '#757272'
+    },
+    separator: {
+        height: 1,
+        width: '100%'
     }
 });
